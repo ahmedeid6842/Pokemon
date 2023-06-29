22261eb3ae761075224cdc21374f75dc671ceb18
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-06-29 06:46:06 +0200

    Fix Linters

--- src/modules/commentListener.js
+++ src/modules/commentListener.js
@@ -1,39 +1,39 @@
-import getComments from "./APIs/comments";
-const involvementURL =
-  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8ev0fUwJNQWCM4y1a4xa/";
-  
-  const buildCommentsList = (comments, commentsList) => {
-    if (comments.length > 0) {
-      commentsList.innerHTML = comments
+import getComments from './APIs/comments';
+
+const involvementURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8ev0fUwJNQWCM4y1a4xa/';
+
+const buildCommentsList = (comments, commentsList) => {
+  if (comments.length > 0) {
+    commentsList.innerHTML = comments
       .map(
         (comment) => `
         <li>
         <p><em>${new Date(comment.creation_date).toLocaleDateString()} 
         ${comment.username}</em> : ${comment.comment} </p>
         </li>
-        `
+        `,
       )
-      .join("");
-    } else {
-      commentsList.innerHTML = "<li>No comments yet</li>";
-    }
-  };
+      .join('');
+  } else {
+    commentsList.innerHTML = '<li>No comments yet</li>';
+  }
+};
 
-  const sendComment = async (pokemonID) => {
-    const commentForm = document.getElementById(`comment-form-${pokemonID}`);
-    const usernameInput = commentForm.querySelector(`#username-pk-${pokemonID}`);
-    const commentInput = commentForm.querySelector(`#comment-pk-${pokemonID}`);
+const sendComment = async (pokemonID) => {
+  const commentForm = document.getElementById(`comment-form-${pokemonID}`);
+  const usernameInput = commentForm.querySelector(`#username-pk-${pokemonID}`);
+  const commentInput = commentForm.querySelector(`#comment-pk-${pokemonID}`);
   const commentListComponent = document.querySelector(
-    `#comments-list-pk-${pokemonID}`
+    `#comments-list-pk-${pokemonID}`,
   );
-  
+
   const errorElement = document.getElementById(`error-${pokemonID}`);
   console.log(errorElement);
-  if (usernameInput.value !== "" && commentInput.value !== "") {
+  if (usernameInput.value !== '' && commentInput.value !== '') {
     // POST Request
     const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         item_id: pokemonID,
         username: usernameInput.value,
@@ -42,76 +42,76 @@ const involvementURL =
     };
     await fetch(
       `${involvementURL}comments?item_id=${pokemonID}`,
-      requestOptions
+      requestOptions,
     );
 
     const comments = await getComments(pokemonID);
     buildCommentsList(comments, commentListComponent);
-    usernameInput.value = "";
-    commentInput.value = "";
+    usernameInput.value = '';
+    commentInput.value = '';
   } else {
     const messages = [];
-    if (usernameInput.value === "" && commentInput.value === "") {
-      messages.push("Please enter a username and a comment.");
-    } else if (commentInput.value === "" && usernameInput.value !== "") {
-      messages.push("Please enter a username.");
-    } else if (usernameInput.value === "" && commentInput.value !== "") {
-      messages.push("Please enter a comment.");
+    if (usernameInput.value === '' && commentInput.value === '') {
+      messages.push('Please enter a username and a comment.');
+    } else if (commentInput.value === '' && usernameInput.value !== '') {
+      messages.push('Please enter a username.');
+    } else if (usernameInput.value === '' && commentInput.value !== '') {
+      messages.push('Please enter a comment.');
     }
 
     if (messages.length > 0) {
-      errorElement.innerText = messages.join(", ");
+      errorElement.innerText = messages.join(', ');
       setTimeout(() => {
-        errorElement.classList.toggle(".hidden");
+        errorElement.classList.toggle('.hidden');
       }, 3000);
     }
   }
 };
 
 export const addCommentButtonListener = () => {
-  const commentBtns = document.querySelectorAll(".comment-btn");
-  const pokemonCards = document.querySelectorAll(".pokemon-card");
-  const ClosepopupBtns = document.querySelectorAll(".close-popup");
+  const commentBtns = document.querySelectorAll('.comment-btn');
+  const pokemonCards = document.querySelectorAll('.pokemon-card');
+  const ClosepopupBtns = document.querySelectorAll('.close-popup');
 
   commentBtns.forEach((commentBtn) => {
-    commentBtn.addEventListener("click", async (event) => {
-      const cardId = event.target.closest(".pokemon-card").id;
+    commentBtn.addEventListener('click', async (event) => {
+      const cardId = event.target.closest('.pokemon-card').id;
       const popup = document.querySelector(`#popup-pk-${cardId}`);
       const commentListComponent = document.querySelector(
-        `#comments-list-pk-${cardId}`
+        `#comments-list-pk-${cardId}`,
       );
 
-      const footer = document.querySelector("footer");
+      const footer = document.querySelector('footer');
       pokemonCards.forEach((card) => {
-        card.classList.add("hidden");
-        footer.classList.toggle("hidden");
+        card.classList.add('hidden');
+        footer.classList.toggle('hidden');
       });
 
-      popup.classList.toggle("hidden");
+      popup.classList.toggle('hidden');
       const comments = await getComments(cardId);
       buildCommentsList(comments, commentListComponent);
     });
   });
 
   ClosepopupBtns.forEach((closeBtn) => {
-    closeBtn.addEventListener("click", (event) => {
+    closeBtn.addEventListener('click', (event) => {
       const popup = event.target.parentNode.parentNode;
-      popup.classList.toggle("hidden");
+      popup.classList.toggle('hidden');
       pokemonCards.forEach((card) => {
-        card.classList.remove("hidden");
+        card.classList.remove('hidden');
       });
     });
   });
 };
 
 export const sendCommentButtonListener = () => {
-  const commentForms = document.querySelectorAll(".comment-form");
+  const commentForms = document.querySelectorAll('.comment-form');
   // console.log(commentForms);
   commentForms.forEach((commentForm) => {
     // console.log(commentForm);
-    const pokemonID = commentForm.id.split("-")[2];
+    const pokemonID = commentForm.id.split('-')[2];
     const submitButton = document.querySelector(`#add-comment-pk-${pokemonID}`);
-    submitButton.addEventListener("click", (event) => {
+    submitButton.addEventListener('click', (event) => {
       event.preventDefault();
       sendComment(pokemonID);
     });
