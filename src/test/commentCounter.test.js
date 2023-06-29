8d842fe05f5403d3bac28dfa8383842c44fdca98
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-06-29 06:56:29 +0200

    Add the test

---
+++ src/test/commentCounter.test.js
@@ -0,0 +1,39 @@
+import { commentsCounter } from '../modules/commentListener';
+
+describe('commentsCounter', () => {
+  let sampCommentCount;
+
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div>
+        <span id="comment-count-1"></span>
+      </div>
+    `;
+    sampCommentCount = document.querySelector('#comment-count-1');
+  });
+
+  test('updates comment count when there are comments', () => {
+    // Arrange
+    const comments = [
+      {id: 1, username: 'user1', comment: 'comment1', creation_date: '2023-06-28'},
+      {id: 2, username: 'user2', comment: 'comment2', creation_date: '2023-06-29'}
+    ];
+
+    // Act
+    commentsCounter('1', comments);
+
+    // Assert
+    expect(sampCommentCount.textContent).toBe('2');
+  });
+
+  test('updates comment count when there are no comments', () => {
+    // Arrange
+    const comments = [];
+
+    // Act
+    commentsCounter('1', comments);
+
+    // Assert
+    expect(sampCommentCount.textContent).toBe('0');
+  });
+});
